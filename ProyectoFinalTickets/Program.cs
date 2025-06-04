3135120f1c3cc208489b0f66d679c489fda212ab
Fabricio Sandoval <sandovalfabri2023@gmail.com>
2025-06-04 11:23:57 -0600

    lineas agregadas

--- ProyectoFinalTickets/Program.cs
+++ ProyectoFinalTickets/Program.cs
@@ -38,6 +38,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Usuario}/{action=Index}/{id?}");
+
 
 app.Run();
