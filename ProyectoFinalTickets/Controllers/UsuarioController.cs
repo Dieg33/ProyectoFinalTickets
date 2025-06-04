7a79f60199f3005ac2b4042ba14e4add07f397c7
Fabricio Sandoval <sandovalfabri2023@gmail.com>
2025-06-04 10:53:16 -0600

    controlador de usuario

---
+++ ProyectoFinalTickets/Controllers/UsuarioController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TuProyecto.Data;
+using TuProyecto.Models;
+
+namespace ProyectoFinalTickets.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsuarioController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View("Registrarse");
+        }
+
+
+        public IActionResult Registrarse()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public IActionResult UsuariosRegistrarse(string nombre,string apellidos,string empresa,string correo)
+        {
+            var nevoUsuario = new Usuario
+            {
+                nombre = nombre,
+                apellidos = apellidos,
+                empresa = empresa,
+                correo = correo
+            };
+            _context.Usuarios.Add(nevoUsuario);
+            _context.SaveChanges();
+            return View();
+        }
+    }
+
+
+    
+   
+
+
+}
