53ac7213330996ea74d3f091d84ad5efb3a3b779
Fabricio Sandoval <sandovalfabri2023@gmail.com>
2025-06-05 00:16:39 -0600

    modelo visto

---
+++ ProyectoFinalTickets/Models/Tickets.cs
@@ -0,0 +1,11 @@
+﻿namespace ProyectoFinalTickets.Models
+{
+    public class Tickets
+    {
+        public int id_ticket { get; set; }
+        public string asunto { get; set; }
+        public DateTime fecha_creacion { get; set; }
+        public string estado { get; set; }
+        public string correo { get; set; }
+    }
+}
